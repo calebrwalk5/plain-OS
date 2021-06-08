03c735c8059ebb51aa788f0fdabbef283f1c6ddf
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-06-08 14:45:29 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,14 +5,25 @@
 #include "./include/vmemory.h"
 #include "./include/time.h"
 #include "./include/types.h"
+#include "./programs/hello.c"
 
 int main() {
   term();
   return 0;
 }
+
 void term() {
   char cmd[30];
-  printf("Welcome to the anusOS shell, enjoy your stay\n");
   printf("root@anusOS> ");
   scanf("%s", cmd);
+  if (cmd == "hello") {
+    hello();
+    break;
+  } else if (cmd == "help") {
+    printf("read through the source code to see what commands you can use\n");
+    break;
+  } else {
+    printf("invalid command\n");
+    break;
+  }
 }
